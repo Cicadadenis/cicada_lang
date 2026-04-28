9d98b48103bcad71c42d30902b1b5b72ec44d46d
Cicada 3301 <50529176+Cicadadenis@users.noreply.github.com>
2026-04-28 17:11:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cicada_lang",
-    version="0.1.2",
+    version="0.1.3",
     description="Язык программирования Cicada",
     author="Cicada3301",
     author_email="email@example.com",
