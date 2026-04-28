7c747d7c7d17307241a77786d4534b753a11befb
Cicada 3301 <50529176+Cicadadenis@users.noreply.github.com>
2026-04-28 15:17:15 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cicada_lang",
+    version="0.1.0",
+    description="Язык программирования Cicada",
+    author="Cicada3301",
+    author_email="email@example.com",
+    url="https://github.com/Cicadadenis/cicada_lang",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.8",
+)
